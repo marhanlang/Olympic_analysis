abab6d1a5c9175e7793580d1184192de4d2ab658
bostican628 <78829871+bostican628@users.noreply.github.com>
2021-08-03 19:24:49 -0500

    Add files via upload

---
+++ Segment_2/pstgrs.sql
@@ -0,0 +1,30 @@
+Create table Summer (
+	Name Varchar,
+	Sex Varchar,
+	Age INT NOT NULL,
+	Height INT NOT NULL,
+	Weight INT NOT NULL,
+	Team Varchar,
+	Games Varchar,
+	Season Varchar,
+	Event Varchar,
+	Medal Varchar
+);
+
+SELECT * From Summer
+
+
+Create table Winter (
+	Name Varchar,
+	Sex Varchar,
+	Age INT NOT NULL,
+	Height INT NOT NULL,
+	Weight INT NOT NULL,
+	Team Varchar,
+	Games Varchar,
+	Season Varchar,
+	Event Varchar,
+	Medal Varchar
+);
+
+SELECT * From Winter
